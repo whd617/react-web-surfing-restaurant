ad5337b284ddd57fd52aa317caf67ad9e5dd2e6b
Jonghyeon Choi <whd617@naver.com>
2024-06-05 21:28:13 +0900

    -chore/useForm 적용하기

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,61 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-export const LoggedOutRouter = () => <span>Logged Out</span>;
+interface IForm {
+  email: string;
+  password: string;
+}
+
+export const LoggedOutRouter = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+
+  const onSubmit = () => {
+    console.log(watch());
+  };
+
+  const onInvalid = () => {
+    console.log('cant create account');
+  };
+
+  return (
+    <div>
+      <h1>Logged out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register('email', {
+              required: 'This is required',
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+          {errors.email?.message && (
+            <span className="font-bold text-red-600">
+              {errors.email?.message}
+            </span>
+          )}
+          {errors.email?.type === 'pattern' && (
+            <span className="font-bold text-red-600">Only gmail allowed</span>
+          )}
+        </div>
+        <div>
+          <input
+            {...register('password', { required: true })}
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Click to login</button>
+      </form>
+    </div>
+  );
+};
