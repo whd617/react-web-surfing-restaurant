26e1f768b4d3dc2e563bfb2b87e15578d867cbac
Jonghyeon Choi <whd617@naver.com>
2024-06-17 10:39:15 +0900

    test/e2e test testing-library/cypress 적용시키기

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000',
   },
   viewportWidth: 1000,
   viewportHeight: 1000,
--- cypress/e2e/first-test.cy.ts
+++ cypress/e2e/first-test.cy.ts
@@ -1,5 +1,18 @@
-describe('First Test', () => {
-  it('should go to homepage', () => {
-    cy.visit('http://localhost:3000').title().should('eq', 'Login | NuberEats');
+describe('Log In', () => {
+  it('should see login page', () => {
+    cy.visit('/').title().should('eq', 'Login | NuberEats');
+  });
+
+  it('can fill out the form', () => {
+    cy.visit('/');
+    cy.findByPlaceholderText(/email/i).type('user@test.com');
+    cy.findByPlaceholderText(/password/i).type('123');
+    cy.findByRole('button').should('not.have.class', 'pointer-events-none');
+    // to do (can log in)
+  });
+  it('can see email / password validation errors', () => {
+    cy.visit('/');
+    cy.findByPlaceholderText(/email/i).type('bad@email');
+    cy.findByRole('alert').should('have.text', 'Please enter a valid email');
   });
 });
