501384124426cd5adf35ba1edb16570e2b8341a8
Jonghyeon Choi <whd617@naver.com>
2024-06-24 19:49:51 +0900

    feat/Driver의 배달위치를 알려주는 google map 기능 구현

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,21 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
+interface ICoords {
+  lat: number;
+  lng: number;
+}
+
 export const Dashboard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  const [map, setMap] = useState<google.maps.Map>();
+  const [maps, setMaps] = useState<any>();
+  const onSuccess = ({
+    coords: { latitude, longitude },
+  }: GeolocationPosition) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+
+  const onError = (error: GeolocationPositionError) => {
+    console.log(error);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode(
+        {
+          location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+        },
+        (result, status) => {
+          console.log(status, result);
+        },
+      );
+    }
+  }, [driverCoords.lat, driverCoords.lng]);
+
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
+  };
+  const onGetRouteClick = () => {
+    if (map) {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({
+        polylineOptions: {
+          strokeColor: '#000',
+          strokeOpacity: 1,
+          strokeWeight: 3,
+        },
+      });
+      directionsRenderer.setMap(map);
+      const request = {
+        origin: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+        destination: new google.maps.LatLng(
+          driverCoords.lat + 0.05,
+          driverCoords.lng + 0.05,
+        ),
+        travelMode: google.maps.TravelMode.DRIVING, // 여기에 적절한 travelMode를 설정하세요
+      };
+
+      directionsService.route(request, (result) => {
+        directionsRenderer.setDirections(result);
+      });
+    }
+  };
+
   return (
     <div>
       <div
         className="  overflow-hidden"
-        style={{ width: window.innerWidth, height: '95vh' }}
+        style={{ width: window.innerWidth, height: '50vh' }}
       >
         <GoogleMapReact
-          defaultZoom={20}
-          defaultCenter={{ lat: 59.95, lng: 30.33 }}
-          bootstrapURLKeys={{ key: 'AIzaSyAwj0IiybKeHnuWgoEK5GbpmVIOQxOSHIQ' }}
-        >
-          <h1>hello</h1>
-        </GoogleMapReact>
+          yesIWantToUseGoogleMapApiInternals={true}
+          onGoogleApiLoaded={onApiLoaded}
+          defaultZoom={16}
+          draggable={false}
+          defaultCenter={{ lat: 36.58, lng: 125.95 }}
+          bootstrapURLKeys={{
+            key: 'AIzaSyAwj0IiybKeHnuWgoEK5GbpmVIOQxOSHIQ',
+          }}
+        ></GoogleMapReact>
       </div>
+      <button onClick={onGetRouteClick}>Get route</button>
     </div>
   );
 };
