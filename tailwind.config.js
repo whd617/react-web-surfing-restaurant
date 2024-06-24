cf66446a4e8b416a53924096f6a89e4b5e72a33e
Jonghyeon Choi <whd617@naver.com>
2024-06-24 23:51:41 +0900

    setup/prod build

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,18 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.tsx'],
   content: [],
+  darkMode: false,
   theme: {
     extend: {
       colors: {
         lime: colors.lime,
       },
     },
+  },
+  variants: {
+    extends: {},
   },
   plugins: [],
 };
